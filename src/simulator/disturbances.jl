7771054613541c252175fdcf106c6461be31c451
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-01-27 13:07:56 -0500

    fix empty dist

--- src/simulator/disturbances.jl
+++ src/simulator/disturbances.jl
@@ -1,15 +1,7 @@
 """
     empty disturbances
 """
-struct EmptyDisturbances{T} <: Disturbances{T}
-    w::Vector{T}
-end
-
-function empty_disturbances(model)
-    EmptyDisturbances(zeros(model.nw))
-end
-
-function disturbances(d::EmptyDisturbances, x, t)
+function disturbances(d::RoboDojo.EmptyDisturbances, x, t)
     return d.w
 end
 
