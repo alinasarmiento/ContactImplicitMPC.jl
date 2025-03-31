768c0b05a30499893acdbcab3a3236c03351dfd9
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-03-31 12:45:41 -0400

    lcmt output debug

--- src/controller/standalone_simulate.jl
+++ src/controller/standalone_simulate.jl
@@ -46,7 +46,9 @@ function callback_sim(lcm, sim, u_lcm_channel)
     return function(channel::String, msg)
         @show channel
         @show msg
+        msg = decode(msg, lmct_robot_output)
         print(msg)
+        
         @infiltrate
         p = sim.policy
         traj = sim.traj
