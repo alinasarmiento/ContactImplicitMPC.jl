748b621fb6561229e18523be0022476750342fd4
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-03-31 15:23:53 -0400

    encode debug

--- src/controller/standalone_simulate.jl
+++ src/controller/standalone_simulate.jl
@@ -78,6 +78,7 @@ function callback_sim(lcm, sim, u_lcm_channel)
         # lcm broadcast p.u
         u = lcmt_robot_input(msg.utime, msg.num_efforts, msg.effort_names, p.u)
         print(u)
+        @infiltrate
         u_lcm = encode(u)
         publish(lcm, u_lcm_channel, u_lcm)
         
