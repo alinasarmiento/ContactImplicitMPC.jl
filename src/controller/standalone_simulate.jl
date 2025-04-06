720419cfed3b7db5ab9ca33552bebf762236d610
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-04-06 18:11:02 -0400

    continue debug broadcast

--- src/controller/standalone_simulate.jl
+++ src/controller/standalone_simulate.jl
@@ -56,7 +56,6 @@ function callback_sim(lcm, sim, u_lcm_channel)
     return function(channel::String, msg)
         # @show channel
         # @show msg
-        @infiltrate
         msg = decode(msg, lcmt_robot_output)
         print(msg)
 
@@ -86,6 +85,7 @@ function callback_sim(lcm, sim, u_lcm_channel)
         u = lcmt_robot_input(msg.utime, msg.num_efforts, msg.effort_names, p.u)
         print(u)
         u_lcm = encode(u)
+        @infiltrate
         publish(lcm, u_lcm_channel, u_lcm)
         
     end
