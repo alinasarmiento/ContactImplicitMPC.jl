d57fdc06f17caadf686e138e707056c18a1ba2a2
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-04-09 14:45:50 -0400

    add time to linear_solve

--- src/solver/lu.jl
+++ src/solver/lu.jl
@@ -7,8 +7,9 @@ function lu_solver(A::SparseMatrixCSC{T,Int}) where T
 end
 
 function linear_solve!(solver::LUSolver{T}, x::Vector{T}, A::SparseMatrixCSC{T,Int},
-        b::Vector{T}; reg::T = 0.0, fact::Bool = true) where T
-    linear_solve!(solver, x, Array(A), b, reg=reg, fact=fact)
+                       b::Vector{T}; reg::T = 0.0, fact::Bool = true) where T
+    print("\n in CIMPC linear_solve \n")
+    @time linear_solve!(solver, x, Array(A), b, reg=reg, fact=fact)
 end
 
 """
