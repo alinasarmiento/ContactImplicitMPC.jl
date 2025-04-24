4e72365ae4eb4f83f60c322affa39797bd91fbcc
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-04-24 14:34:36 -0400

    add back H_mpc in reset!

--- src/controller/newton.jl
+++ src/controller/newton.jl
@@ -133,7 +133,7 @@ function reset!(core::Newton, ref_traj::ContactTraj,
     warm_start::Bool = false) where T
 
     # H = ref_traj.H
-    # H_mpc = core.traj.H
+    H_mpc = core.traj.H
     opts = core.opts
 
     # Reset β value
