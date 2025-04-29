9dc39e8319a2463d9712abd8becffe5d80d70900
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-04-29 13:52:14 -0400

    debug

--- src/controller/newton.jl
+++ src/controller/newton.jl
@@ -133,8 +133,6 @@ function reset!(core::Newton, ref_traj::ContactTraj,
     warm_start::Bool = false) where T
 
     # H = ref_traj.H
-    print(core.traj.H)
-    print("\n")
     H_mpc = core.traj.H
     opts = core.opts
 
