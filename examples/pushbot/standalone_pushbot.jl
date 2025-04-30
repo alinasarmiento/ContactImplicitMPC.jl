a2f6daacfd74ba23be53431e5e204d605598ea7e
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-04-30 18:43:25 -0400

    segfault debug, remove msg

--- examples/pushbot/standalone_pushbot.jl
+++ examples/pushbot/standalone_pushbot.jl
@@ -101,7 +101,11 @@ sim = simulator(s, H_sim, h=h_sim, policy=p, dist=d)
 
 # set up for warm start
 newton_solve!(sim.policy.newton, sim.policy.s, sim.policy.q0, q1,
-                        sim.policy.im_traj, sim.policy.traj, warm_start = false)
+              sim.policy.im_traj, sim.policy.traj, warm_start = false)
+print("\n sim.policy outside lcm:\n")
+print(propertynames(sim.policy))
+print("\n")
+print(sim.policy.q0)
 print("\n ready \n")
 
 ## LCM + Drake loop
