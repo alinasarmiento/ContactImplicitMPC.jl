89c46a41f6c1b31e80081747a2ca6ac35b3d63e3
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-05-05 16:24:00 -0400

    typo bug

--- src/controller/standalone_simulate.jl
+++ src/controller/standalone_simulate.jl
@@ -93,14 +93,11 @@ function callback_sim(lcm, sim, u_lcm_channel)
     end
 end
 
-lcmt = pyimport("lcmtypes.dairlib")
+# lcmt = pyimport("lcmtypes.dairlib")
 function callback_sim_py(sim, u_lcm_channel)
     return function(channel::String, msg)
         print("\n received")
-        print(lcmt)
-        print(lcmt.lcmt_robot_output)
-        print(lcmt.lcmt_robot_output.decode)
-        msg = lcmt.lcmt_robot_output.decode(msg)
+        msg = 0 #lcmt.lcmt_robot_output.decode(msg)
         print("\n x: ")
         print(msg)
         print("\n position\n")
