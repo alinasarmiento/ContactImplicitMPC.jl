5043b0cf1efbad033694344c614fb0edf6901758
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-02 18:18:06 -0400

    change pushbot dt

--- examples/pushbot/standalone_pushbot.jl
+++ examples/pushbot/standalone_pushbot.jl
@@ -50,26 +50,26 @@ status = simulate!(sim, q1, v1)
 
 ## MPC setup 
 N_sample = 2
-H_mpc = 40
+H_mpc = 400
 h_sim = 0.0005 #h / N_sample
 H_sim = 1000
 κ_mpc = 1.0e-4
 
 ## Slow Recovery
-obj = TrackingVelocityObjective(model, env, H_mpc,
-	q = [Diagonal([120*(t/H_mpc)^2; 10.0*(t/H_mpc)^4]) for t = 1:H_mpc-0],
-	v = [Diagonal([1; 0.0] ./ (h^2.0)) for t = 1:H_mpc-0],
-	u = [Diagonal([100; 0.0]) for t = 1:H_mpc-0],
-	γ = [Diagonal(1.0e-100 * ones(model.nc)) for t = 1:H_mpc-0],
-	b = [Diagonal(1.0e-100 * ones(model.nc * friction_dim(env))) for t = 1:H_mpc]);
+# obj = TrackingVelocityObjective(model, env, H_mpc,
+# 	q = [Diagonal([120*(t/H_mpc)^2; 10.0*(t/H_mpc)^4]) for t = 1:H_mpc-0],
+# 	v = [Diagonal([1; 0.0] ./ (h^2.0)) for t = 1:H_mpc-0],
+# 	u = [Diagonal([100; 0.0]) for t = 1:H_mpc-0],
+# 	γ = [Diagonal(1.0e-100 * ones(model.nc)) for t = 1:H_mpc-0],
+# 	b = [Diagonal(1.0e-100 * ones(model.nc * friction_dim(env))) for t = 1:H_mpc]);
 
 ## Fast Recovery
-# obj = TrackingVelocityObjective(model, env, H_mpc,
-#     q = [Diagonal([12*(t/H_mpc)^2; 12*(t/H_mpc)^2]) for t = 1:H_mpc-0],
-# 	v = [Diagonal([1; 0.01] ./ (h^2.0)) for t = 1:H_mpc-0],
-#     u = [Diagonal([100; .0001]) for t = 1:H_mpc-0],
-#     γ = [Diagonal(1.0e-100 * ones(model.nc)) for t = 1:H_mpc-0],
-#     b = [Diagonal(1.0e-100 * ones(model.nc * friction_dim(env))) for t = 1:H_mpc]);
+obj = TrackingVelocityObjective(model, env, H_mpc,
+    q = [Diagonal([12*(t/H_mpc)^2; 12*(t/H_mpc)^2]) for t = 1:H_mpc-0],
+	v = [Diagonal([1; 0.01] ./ (h^2.0)) for t = 1:H_mpc-0],
+    u = [Diagonal([100; .0001]) for t = 1:H_mpc-0],
+    γ = [Diagonal(1.0e-100 * ones(model.nc)) for t = 1:H_mpc-0],
+    b = [Diagonal(1.0e-100 * ones(model.nc * friction_dim(env))) for t = 1:H_mpc]);
 
 ## Policy
 p = ci_mpc_policy(ref_traj, s, obj,
@@ -94,7 +94,7 @@ p = ci_mpc_policy(ref_traj, s, obj,
 # d = impulse_disturbances(impulses, idx);
 
 ## Initial Conditions
-q1_sim = [0.0, 0.0]
+q1_sim = [0.2, 0.0]
 v1_sim = [0.0; 0.0]
 
 ## Simulator
