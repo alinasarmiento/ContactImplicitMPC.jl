0667cf5340d53ccd920c37d3f2c866877ba32a24
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-03 14:05:16 -0400

    changing generate functions

--- examples/generate_examples.jl
+++ examples/generate_examples.jl
@@ -8,8 +8,8 @@ include(joinpath(pkg_dir, "src/code_gen_loader.jl"))
 
 # Generate dynamics
 dynamicsdir = joinpath(dirname(pathof(ContactImplicitMPC)), "..", "src", "dynamics")
-include(joinpath(dynamicsdir, "generate_dynamics.jl"))
+include(joinpath(dynamicsdir, "generate_pushbot_dynamics.jl"))
 
 # Generate simulation
 simulationdir = joinpath(dirname(pathof(ContactImplicitMPC)), "..", "src", "simulation")
-include(joinpath(simulationdir, "generate_simulation.jl"))
+include(joinpath(simulationdir, "generate_pushbot_simulation.jl"))
--- examples/pushbot/standalone_pushbot.jl
+++ examples/pushbot/standalone_pushbot.jl
@@ -50,7 +50,7 @@ status = simulate!(sim, q1, v1)
 
 ## MPC setup 
 N_sample = 2
-H_mpc = 100
+H_mpc = 200
 h_sim = 0.005 #h / N_sample
 H_sim = 1000
 κ_mpc = 1.0e-4
