f2c3070eb85579a1fa04589635950ee51fdcf540
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-21 20:48:52 -0400

    reverted

--- src/dynamics/pushbot/model.jl
+++ src/dynamics/pushbot/model.jl
@@ -29,8 +29,8 @@ function _kinematics(model::PushBot, q; mode = :com)
 		return [-model.l * sin(θ) + d * cos(θ);
 				 model.l * cos(θ) + d * sin(θ)]
 	elseif mode == :com
-		return [-1.0 * (model.l/2) * sin(θ);
-				 1.0 * (model.l/2) * cos(θ)]
+		return [-1.0 * model.l * sin(θ);
+				 1.0 * model.l * cos(θ)]
 	elseif mode == :ee
 		return [-model.l * sin(θ);
 				 model.l * cos(θ)]
@@ -47,8 +47,8 @@ function _jacobian(model::PushBot, q; mode = :com)
  		return [(-model.l * cos(θ) - d * sin(θ)) cos(θ);
 		        (-model.l * sin(θ) + d * cos(θ)) sin(θ)]
  	elseif mode == :com
- 		return [-1.0 * (model.l/2) * cos(θ) 0.0;
-		        -1.0 * (model.l/2) * sin(θ) 0.0]
+ 		return [-1.0 * model.l * cos(θ) 0.0;
+		        -1.0 * model.l * sin(θ) 0.0]
 	elseif mode == :ee
 		return [-model.l * cos(θ) 0.0;
 		        -model.l * sin(θ) 0.0]
