9299f6883dabcbbedcb693333fb14515b8d7308f
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-24 14:39:26 -0400

    w

--- src/dynamics/waiter2d/model.jl
+++ src/dynamics/waiter2d/model.jl
@@ -158,7 +158,7 @@ function contact_forces(model::Waiter2D, env::Environment{<:World, LinearizedCon
     SVector{8}([transpose(rotation(env, k[1:1])) * [m * b1[1:2]; γ1[1]];
                 transpose(rotation(env, k[3:3])) * [m * b1[3:4]; γ1[2]];
                 transpose(rotation(env, k[5:5])) * [m * b1[5:6]; γ1[3]];
-                transpose(rotation(env, k[7:7])) * [m * b1[7:8]; γ1[4]];)
+                transpose(rotation(env, k[7:7])) * [m * b1[7:8]; γ1[4]];])
 end
 
 function velocity_stack(model::Waiter2D, env::Environment{<:World, LinearizedCone}, q1, q2, k, h)
