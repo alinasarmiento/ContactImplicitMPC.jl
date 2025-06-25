01fbfd75412d631aa7213bdbde345f25df91f5c8
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-25 16:10:46 -0400

    Revert "jacobian"
    This reverts commit 0b7965a98408b13b2b90aa3637d82043aae637ff.

--- src/controller/implicit_dynamics.jl
+++ src/controller/implicit_dynamics.jl
@@ -29,7 +29,8 @@ function ImplicitTrajectory(ref_traj::ContactTraj, s::Simulation;
 	    r_tol = 1.0e-8,
 	    diff_sol = true,
 	    solver = :empty_solver,
-	    max_time = max_time))
+	    max_time = max_time,
+            verbose = true))
 
 	model = s.model
 	env = s.env
@@ -65,7 +66,7 @@ function ImplicitTrajectory(ref_traj::ContactTraj, s::Simulation;
 			 r  = RLin(s, lin[t].z, lin[t].θ, lin[t].r, lin[t].rz, lin[t].rθ),
 			 rz = RZLin(s, lin[t].rz),
 			 rθ = RθLin(s, lin[t].rθ),
-			 opts = opts) for t = 1:H]
+			 opts = opts,verbose=true) for t = 1:H]
 
 	# views
 	d = [view(ip[t].z, 1:nd) for t = 1:H]
