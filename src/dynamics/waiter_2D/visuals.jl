d061fb15583ffbae715c969b7e8145ac9f41872e
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-06-24 21:31:14 -0400

    w

--- src/dynamics/waiter_2D/visuals.jl
+++ src/dynamics/waiter_2D/visuals.jl
@@ -47,7 +47,7 @@ function build_robot!(vis::Visualizer, model::Waiter2D; name::Symbol=:Waiter2D, 
                                        convert(Float32, r)),
 	       body_mat)
     setobject!(vis[name][:robot]["contact_front"], Sphere(GeometryBasics.Point3f0(0.0),0.01),contact_mat)
-    setobject!(vis[name][:robot]["contact_back"], Sphere(GeomeryBasics.Point3f0(0.0),0.01),contact_mat)
+    setobject!(vis[name][:robot]["contact_back"], Sphere(GeometryBasics.Point3f0(0.0),0.01),contact_mat)
 
     setobject!(vis[name][:object]["tray"],
                GeometryBasics.Cylinder(GeometryBasics.Point3f0(0,0,-params["d_tray"]/2),
