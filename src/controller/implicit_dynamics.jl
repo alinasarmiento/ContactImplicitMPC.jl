d53ff90845bb9d2cb213d482db8bc9b0da641e8a
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-07-01 16:03:31 -0400

    change error message

--- src/controller/implicit_dynamics.jl
+++ src/controller/implicit_dynamics.jl
@@ -162,7 +162,6 @@ function implicit_dynamics!(im_traj::ImplicitTrajectory, traj::ContactTraj)
 	im_traj.ip[t].θ .= traj.θ[t]
 
 	# solve
-        im_traj.ip[t].opts.verbose = true
 	status = interior_point_solve!(im_traj.ip[t])
         
 	!status && (@warn "implicit dynamics failure (t = $t)")
