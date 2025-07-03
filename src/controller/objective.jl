9b2275fd03ca67f683e380d6b99d158a0bf08af9
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-07-03 19:35:38 -0400

    ?

--- src/controller/objective.jl
+++ src/controller/objective.jl
@@ -32,7 +32,7 @@ function TrackingVelocityObjective(model, env, H::Int;
     return TrackingVelocityObjective(q, v, u, γ, b)
 end
 
-function eval_obj(model, env, im_traj::ImplicitTrajectory, H::Int, obj::Objective, t::T)
+function eval_obj(model, env, im_traj::ImplicitTrajectory, H::Int, obj::Objective, t::Int)
     z = im_traj.lin[t].z
     q2, γ1, b1, ψ1, s1, η1, s2 = unpack_z(model, env, z)
     print(z)
