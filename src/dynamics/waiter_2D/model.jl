c2c7f739cd1cc708d96b6896053995d1123a0ec3
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-07-10 21:13:45 -0400

    nvm

--- src/dynamics/waiter_2D/model.jl
+++ src/dynamics/waiter_2D/model.jl
@@ -85,8 +85,8 @@ function M_func(model::Waiter2D, q)
     mt = model.m_tray
     h_t = model.d_tray
     w_t = 2*model.r_tray
-    # I_tray = (1/12)*mt*(h_t^2 + w_t^2)
-    I_tray = 0.0
+    I_tray = (1/12)*mt*(h_t^2 + w_t^2)
+    # I_tray = 0.0
 
     Diagonal(@SVector [m, m, mt, mt, I_tray])
 end
