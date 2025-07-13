8d5a889535caef04da32a980ee248064f2eb9af9
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-07-13 13:59:46 -0400

    nvm. halfplane sdf

--- src/dynamics/waiter_2D/model.jl
+++ src/dynamics/waiter_2D/model.jl
@@ -109,21 +109,21 @@ function dist_tray(model::Waiter2D, p, pt)
     xdiff,zdiff = R*(differ)
 
     ## just halfplane
-    # zdist = -zdiff - model.d_tray/2q
+    zdist = -zdiff - model.d_tray/2q
     
     ## full
-    xdiff = abs(xdiff)
-    zdiff = abs(zdiff)        
-    zdist = zdiff-(model.d_tray/2)
-    xdist = xdiff-model.r_tray
+    # xdiff = abs(xdiff)
+    # zdiff = abs(zdiff)        
+    # zdist = zdiff-(model.d_tray/2)
+    # xdist = xdiff-model.r_tray
 
-    dist_neg = min(0, max(xdist, zdist))
+    # dist_neg = min(0, max(xdist, zdist))
     
-    zdist = max(0, zdist)
-    xdist = max(0, xdist)
+    # zdist = max(0, zdist)
+    # xdist = max(0, xdist)
     
-    return norm([xdist, zdist]) + dist_neg
-   # return zdist
+    # return norm([xdist, zdist]) + dist_neg
+   return zdist
 end
 
 # signed distance function
