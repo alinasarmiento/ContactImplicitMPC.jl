9d2fb980a8f1d9b84b4419a162fd16c91808043d
Grey Sarmiento <alina.t.sarmiento@gmail.com>
2025-09-05 11:08:24 -0400

    debug q/u limits

--- src/dynamics/waiter_2D/model.jl
+++ src/dynamics/waiter_2D/model.jl
@@ -270,8 +270,8 @@ waiter_2D = Waiter2D(5, 2, 2, 6,
 	             SVector{5}(zeros(5)),
                      SVector{2}([-20, -20]), # u min
                      SVector{2}([20, 20]),   # u max
-                     SVector{2}([0.1, 0.15, 1,1,1]), # q min
-                     SVector{2}([0.6, 0.9, 1,1,1]),   # q max
+                     SVector{5}([0.1, 0.15, 1,1,1]), # q min
+                     SVector{5}([0.6, 0.9, 1,1,1]),   # q max
 )
 
 function friction_coefficients(model::Waiter2D) 
